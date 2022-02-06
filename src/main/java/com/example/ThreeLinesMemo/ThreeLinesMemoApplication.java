cf3db20bfe047b4bf48a404585fb51edc660ceb4
sukesan000 <oyama.k.1997@gmail.com>
2022-02-06 14:49:52 +0900

    lesson commit

---
+++ src/main/java/com/example/ThreeLinesMemo/ThreeLinesMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThreeLinesMemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeLinesMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeLinesMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThreeLinesMemo/ThreeLinesMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThreeLinesMemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeLinesMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
